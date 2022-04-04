12700a10dbaaa00625538bbb31c263c196ace9df
fzerbo <franciszerbo98@gmail.com>
2022-04-03 21:15:06 -0300

    Creando el primer commit

---
+++ ejercitacion.js
@@ -0,0 +1,17 @@
+let primerNumero = Number();
+let segundoNumero = Number();
+let resultadoSuma;
+let resultadoResta;
+let resultadoDivision;
+let resultadoMultiplicacion;
+
+primerNumero = '10';
+segundoNumero = '20';
+
+resultadoSuma = (primerNumero) + (segundoNumero);
+
+resultadoResta = (primerNumero) - (segundoNumero);
+
+resultadoMultiplicacion = (primerNumero) * (segundoNumero);
+
+resultadoDivision = (primerNumero) / (segundoNumero);
